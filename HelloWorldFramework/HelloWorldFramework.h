11c21ae291452d72f8ffb1166c2f87f682b6f156
naotofu <naotoufu@outlook.com>
2021-04-05 17:06:49 +0900

    Initial Commit

---
+++ HelloWorldFramework/HelloWorldFramework.h
@@ -0,0 +1,18 @@
+//
+//  HelloWorldFramework.h
+//  HelloWorldFramework
+//
+//  Created by Naoto Ito on 2021/04/05.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloWorldFramework.
+FOUNDATION_EXPORT double HelloWorldFrameworkVersionNumber;
+
+//! Project version string for HelloWorldFramework.
+FOUNDATION_EXPORT const unsigned char HelloWorldFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloWorldFramework/PublicHeader.h>
+
+
